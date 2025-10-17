5fe6ca94761f3c7775fbee84ce97bec0d2012eb5
Opilane TTHK <opilane@tthk.ee>
2025-10-17 11:14:21 +0300

    SwitchWithNumbers & Flowchart

---
+++ 17.10.2025/SwitchWithNumbers/SwitchWithNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SwitchWithNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number 1,2 või 3");
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    Console.Beep();
+                    break;
+                case "2":
+                    Console.Beep();
+                    Console.Beep();
+                    break;
+                case "3":
+                    Console.Beep();
+                    Console.Beep();
+                    Console.Beep();
+                    break;
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+        }
+    }
+}
