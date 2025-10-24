0af9f76dccc9b1bfccefc0888001c49f9dcc9819
Opilane TTHK <opilane@tthk.ee>
2025-10-24 11:30:26 +0300

    SwitchWeekDay & Flowchart

---
+++ SwitchWeekDay/SwitchWeekDay/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SwitchWeekDay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //tuleb kasutada switch lauset
+            //kui nädalapäev on esmaspäev , siis prinditakse "Tere esmaspäev!"
+            //peate kasutama DayOfWeek enumeratsiooni
+
+            switch (DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    Console.WriteLine("Tere Esmaspäev!");
+                    break;
+                case DayOfWeek.Tuesday:
+                    Console.WriteLine("Teisipäev");
+                    break;
+                case DayOfWeek.Wednesday:
+                    Console.WriteLine("Kolmapäev");
+                    break;
+                case DayOfWeek.Thursday:
+                    Console.WriteLine("Neljapäev");
+                    break;
+                case DayOfWeek.Friday:
+                    Console.WriteLine("Reede");
+                    break;
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("Laupäev");
+                    break;
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("Pühapäev");
+                    break;
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+        }
+    }
+}
