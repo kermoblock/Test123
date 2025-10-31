3e1d986b6dfd82a644f0988d33df0074ba378f01
Opilane TTHK <opilane@tthk.ee>
2025-10-31 09:44:11 +0200

    SwitchMultipleCases & Flowchart

---
+++ 31.10.2025/SwitchMultipleCases/SwitchMultipleCases/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace SwitchMultipleCases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //sisestad konsooli väärtuse
+            //loeb sisestatud väärtuse stringina
+            //tuleb kasutada switchi
+            //esimeses cases on väärtused "Intro to C#", "Variables",
+            //Data Types ja väärtuseks on Basic
+            //Teises cases on väärtused "OOP", "Classes",
+            //"Objects" ja vastuseks Intermediate
+            //Kolmandas cases on väärtused "Asynchronous programming", "LINQ",
+            //"Delegates" ja vastuseks Advanced
+
+            string topic, category;
+
+            topic = Console.ReadLine();
+
+            switch (topic)
+
+            {
+                case "Intro to C#":
+                case "Variables":
+                case "Data Types":
+                    category = "Basic";
+                    break;
+
+                case "OOP":
+                case "Classes":
+                case "Objects":
+                    category = "Intermediate";
+                    break;
+
+                case "Asychronous prorgramming":
+                case "LINQ":
+                case "Delegates":
+                    category = "Advanced";
+                    break;
+
+                default:
+                    category = "Unknown";
+                        break;
+            }
+            Console.WriteLine("Category is " + category);
+        }
+    }
+}
