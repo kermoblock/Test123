08fa0a86ed5f71d5d475a8a449e615138c5c7d35
Opilane TTHK <opilane@tthk.ee>
2025-10-31 11:34:28 +0200

    SwitchNumbers

---
+++ 31.10.2025/SwitchNumbers/SwitchNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SwitchNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //peab saama sisestada numbrit ja muudab selle int admetüübiks
+
+            //tuleb teha swtich, kus esimene case 10, teine case 20 ja kolmas case 30
+            //kindlasti tuleb ka deafault lõppu tehas
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            switch (number)
+            {
+                case 10:
+                    Console.WriteLine("Sisestasid numbri 10");
+                    break;
+
+                case 20:
+                    Console.WriteLine("Sisestasid numbri 20");
+                    break;
+
+                case 30:
+                    Console.WriteLine("Sisestasid numbri 30");
+                    break;
+
+                default:
+                    Console.WriteLine("Sisestasid vale numbri");
+                    break;
+            }
+        }
+    }
+}
