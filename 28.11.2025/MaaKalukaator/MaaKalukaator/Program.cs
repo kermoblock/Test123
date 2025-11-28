4952ba81babd25e41edc3bc572b67b27dde11bcd
Opilane TTHK <opilane@tthk.ee>
2025-11-28 11:23:18 +0200

    MaaKalkulaator

---
+++ 28.11.2025/MaaKalukaator/MaaKalukaator/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MaaKalukaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //meil on münt, mille diameeter on 25.75mm
+            //arvutada välja maa ümbermõõt ja mitu münti
+            //mahub järjestiku ümber maa
+            //arvutades tohib kasutada ainult mündi diameetrit ja maa raadiust
+
+            //maa ümbermõõt:
+            long maaraadius = 6371000000;
+            Console.WriteLine("Maa ümbermõõt on " + 2 * Math.PI * maaraadius + " millimeetrit");
+
+            //mitu münti mahub:
+            double mündidiameeter = 25.75;
+            double mündid = maaraadius / mündidiameeter;
+            Console.WriteLine("Maa ümber mahub {0:0} münti", mündid);
+        }
+    }
+}
