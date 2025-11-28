37903af66246cb0f974eb2d2bfe01f9884a373e0
Opilane TTHK <opilane@tthk.ee>
2025-11-28 11:43:42 +0200

    ComputerCalcLogic

---
+++ 28.11.2025/ComputerCalcLogic/ComputerCalcLogic/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ComputerCalcLogic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Kui palju on 3 + 8 / (4 - 2) * 4 = ?");
+
+            double calculation1 = 3 + 8 / (4 - 2) * 4;
+            float calculation2 = 3 + 8 / 4 - 2 * 4;
+            double calculation3 = (3 + 8) / (4 - 2) * 4;
+
+            double calculation4 = ((3.0 + 8.0) / (4.0 - 2.0) * 4.0);
+
+
+            Console.WriteLine("3 + 8 / (4 - 2) * 4 = " + calculation1);
+            Console.WriteLine("3 + 8 / 4 - 2 * 4 = " + calculation2);
+            Console.WriteLine("(3 + 8) / (4 - 2) * 4 = " + calculation3);
+            Console.WriteLine("((3.0 + 8.0) / (4.0 - 2.0) * 4.0) = " + calculation4);
+        }
+    }
+}
