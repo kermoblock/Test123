95d18f67fe845b60688de6e74271b9aca18673f2
Opilane TTHK <opilane@tthk.ee>
2025-12-05 11:41:01 +0200

    ForKillKoll

---
+++ 05.12.2025/ForKillKoll/ForKillKoll/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ForKillKoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int arv = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < arv; i++)
+            {
+
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine("Kill-Koll");
+                }
+
+                for (int k = 0; k < 1; k++)
+                {
+                    Console.WriteLine("Killadi-Koll");
+                }
+            }
+            Console.WriteLine("Kill-Koll");
+        }
+    }
+}
