f384dd22372664896125909d8794a6bb9c5b5a95
Opilane TTHK <opilane@tthk.ee>
2025-12-05 09:02:01 +0200

    ForLoopHello

---
+++ 05.12.2025/ForLoopHello/ForLoopHello/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ForLoopHello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //teha konooli rakendus kus kasutad for loopi
+            //kolm korda näidatakse Tere Tulemast
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Tere Tulemast");
+            }
+        }
+    }
+}
