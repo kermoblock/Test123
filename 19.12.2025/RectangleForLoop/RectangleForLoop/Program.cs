7cfdad03cf5c02f0e04cf67fdb8c8a269d224b44
Opilane TTHK <opilane@tthk.ee>
2025-12-19 11:40:18 +0200

    RectangleForLoop

---
+++ 19.12.2025/RectangleForLoop/RectangleForLoop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+
+namespace RectangleForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta ristküliku kahe külje mõõdud");
+            Console.WriteLine("Pikus");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Laius");
+            double width = double.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= length; row++)
+            {
+                for (int col = 1; col <= width; col++)
+                {
+                    string mark;
+                    if (row == col || row + col <= +1)
+                    {
+                        mark = "*";
+                    }
+                    else
+                    {
+                        mark = "*";
+                    }
+                    Console.Write(mark);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
