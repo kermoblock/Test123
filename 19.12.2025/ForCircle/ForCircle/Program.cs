8275552c6098162f3e89cf509884a9849bcd9d42
Opilane TTHK <opilane@tthk.ee>
2025-12-19 10:22:16 +0200

    ForCircle

---
+++ 19.12.2025/ForCircle/ForCircle/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ForCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            double thickness = 0.4;
+            char symbol = '*';
+
+            do
+            {
+                Console.Write("Enter radius:::: ");
+                if (!double.TryParse(Console.ReadLine(), out radius) || radius <= 0)
+                {
+                    Console.WriteLine("Radius have to be positive number");
+                }
+            }
+            while (radius <= 0);
+
+            Console.WriteLine();
+            double rIn = radius - thickness, rOut = radius + thickness;
+
+            for (double y = radius; y >= -radius; --y)
+            {
+                for (double x = -radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.Write(symbol);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
+
+
+
